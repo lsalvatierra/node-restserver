90f319423a1e099dc0adf4cc9f38ab90f41fbb85
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2022-07-18 16:03:38 -0500

    new changes

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -7,25 +7,35 @@ const Usuario = require('../models/usuario');
 
 const usuariosGet = async (req = request, res = response) => {
     //const {nombre, apellido= 'Sin Apellido'} = req.query;
-    const {limite = 5, desde = 0 } = req.query;
-    const query = { estado: true};
+    //const {limite = 5, desde = 0 } = req.query;
+    const {page = 1, perPage = 5 } = req.query;
+    const fil = 'te';
+    const query = { estado: true  };
     //Esta forma es menos optima
     // const usuarios = await Usuario.find(query)
     //                     .skip(Number(desde))
     //                     .limit(Number(limite));
     //const total = await Usuario.countDocuments(query);
     //El resultado va esperar que la promesa concluya  con las dos operaciones terminadas.
+    var numberPerPage = parseInt(perPage);
+    const desde = (page-1) * numberPerPage;
+    
     const [total, usuarios] = await Promise.all([
         Usuario.countDocuments(query),
         Usuario.find(query)
         .skip(Number(desde))
-        .limit(Number(limite))
+        .limit(Number(numberPerPage))
     ]);
-
+    const totalPages = Math.ceil(total / perPage);
+    var paginationData = {"perPage": numberPerPage, totalPages, total };  
     res.json({
-        total,
+        paginationData,
         usuarios
     })
+    // res.json({
+    //     total,
+    //     usuarios
+    // })
 }; 
 
 
